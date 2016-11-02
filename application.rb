5529b7dc74d9c1503076a97742269aeb6d5f3900
Danny Smith <hi@danny.is>
2016-11-02 14:11:09 +0000

    Fixed bug by duplicating hash

--- application.rb
+++ application.rb
@@ -22,19 +22,24 @@ class Flashcards < Sinatra::Base
     erb :index
   end
 
+  get '/cards/?' do
+    redirect '/'
+  end
+
   get '/cards.json' do
     content_type :json
     status 200
-    $flashcards.each do |k,v|
+    @flashcards = Marshal.load Marshal.dump($flashcards) # Deep Clone hash
+    @flashcards.each do |k,v|
       v["questions"].each do |question|
         unless question["answer"].respond_to? :each
           if question["answer"].match(/image\[(.+)\]/)
-            question["answer"] = question["answer"].gsub(/image\[/, "http://foo.com/images/")[0..-2]
+            question["answer"] = question["answer"].gsub(/image\[/, "#{$base_url}/images/")[0..-2]
           end
         end
       end
     end
-    $flashcards.to_json
+    @flashcards.to_json
   end
 
   get '/cards/:flashcard' do
